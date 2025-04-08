54b9dc65733f3a2403606fd4ead70bbea1100081
Juan Diego Simales Mesa <juandiegosimalesmesa@gmail.com>
2025-04-08 17:33:31 -0500

    Connecting DB - HTTP:GET

--- src/controllers/categorias.controllers.js
+++ src/controllers/categorias.controllers.js
@@ -1,5 +1,14 @@
+import getConnection  from "./../db/database.js"
 const getcategorias = async (req, res) => {
-res.json({"categorias": "Electrodomesticos"})
+    try {
+        const connection = await getConnection();
+        const result = await connection.query("SELECT 	id_admin, 	nombre_admin, email, contrasena, rol, fecha_creacion  FROM administradores")
+        res.json(result);
+    } catch (error) {
+        
+    }
+ 
+    
 }
 
 export const methodHTTP = {
